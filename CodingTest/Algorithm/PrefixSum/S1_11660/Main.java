f635d789abc6e68aef0e5d0c0f222e651efc7281
keartt <shalpha_2@naver.com>
2024-06-26 00:57:15 +0900

    240626 [구간합] S1 백준 풀이
    2차원 구간 내의 구간의 합 구하기
    2차원 배열 구간합과 구간내의 구간합 구하는 문제

---
+++ CodingTest/Algorithm/PrefixSum/S1_11660/Main.java
@@ -0,0 +1,40 @@
+package CodingTest.Algorithm.PrefixSum.S1_11660;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] S = new int[N+1][N+1];
+        for (int i = 1; i < N+1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < N+1; j++) {
+                S[i][j] = Integer.parseInt(st.nextToken()) + S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1 - 1][y1 - 1];
+            bw.write(result + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
