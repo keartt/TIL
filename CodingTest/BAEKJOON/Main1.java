d46612b1a7a7cab791c30b53631ba7841dc73293
keartt <shalpha_2@naver.com>
2024-07-02 18:54:13 +0900

    240702 [알고리즘] Stack & Q

---
+++ CodingTest/BAEKJOON/Main1.java
@@ -0,0 +1,68 @@
+package CodingTest.BAEKJOON;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        int last = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String input = st.nextToken();
+            switch (input) {
+                case "push":
+                    last = Integer.parseInt(st.nextToken());
+                    q.add(last);
+                    break;
+                case "pop":
+                    if (q.isEmpty()) {
+                        sb.append(-1);
+                    }else{
+                        sb.append(q.poll());
+                    }
+                    break;
+                case "size":
+                    sb.append(q.size());
+                    break;
+                case "empty":
+                    sb.append(q.isEmpty() ? 1 : 0);
+                    break;
+                case "front":
+                    if (q.isEmpty()) {
+                        sb.append(-1);
+                    }else{
+                        sb.append(q.peek());
+                    }
+                    break;
+                case "back":
+                    if (q.isEmpty()) {
+                        sb.append(-1);
+                    }else{
+//                        int qsize = q.size();
+//                        for (int j = 0; j < qsize - 1; j++) {
+//                            q.add(q.remove());
+//                        }
+//                        int last = q.poll();
+//                        q.add(last);
+                        sb.append(last);
+                    }
+                    break;
+            }
+            if (!input.equals("push")) sb.append("\n");
+        }
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+    }
+
+}
---
+++ CodingTest/BAEKJOON/Main2.java
@@ -0,0 +1,47 @@
+package CodingTest.BAEKJOON;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String input = st.nextToken();
+            switch (input) {
+                case "push":
+                    s.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    sb.append(s.isEmpty() ? -1 : s.pop());
+                    break;
+                case "size":
+                    sb.append(s.size());
+                    break;
+                case "empty":
+                    sb.append(s.isEmpty() ? 1 : 0);
+                    break;
+                case "top":
+                    sb.append(s.isEmpty() ? -1 : s.peek());
+                    break;
+            }
+            if (!input.equals("push")) sb.append("\n");
+        }
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+    }
+
+}
