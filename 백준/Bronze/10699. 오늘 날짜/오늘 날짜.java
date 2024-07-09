525818e6185f1bd31d42cca2b5152ee53c004377
keartt <83094369+keartt@users.noreply.github.com>
2024-07-09 15:27:21 +0900

    [Bronze V] Title: 오늘 날짜, Time: 196 ms, Memory: 17040 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate today = java.time.LocalDate.now();
+        System.out.println(today);
+    }
+}
