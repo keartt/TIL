2a55809da55e701ae2ab55a01f1995433293cab4
keartt <shalpha_2@naver.com>
2024-07-13 22:10:14 +0900

    240713 백준 문제풀이 정리
    1018 체스판 다시 칠하기
    블로그에도 정리함

---
+++ CodingTest/BAEKJOON/Silver/S4_1018/Main.java
@@ -0,0 +1,47 @@
+package CodingTest.BAEKJOON.Silver.S4_1018;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static String[] board;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        board = new String[N];
+        for (int i = 0; i < N; i++) {
+            board[i] = br.readLine();
+        }
+
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                int count = solution(i, j);
+                if (result > count) result = count;
+            }
+        }
+        bw.write(result + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+    private static int solution(int startRow, int endRow) {
+        String[] answer = {"BWBWBWBW", "WBWBWBWB"};
+        int count = 0;
+        for (int i = 0; i < 8; i++) {
+            int row = startRow + i;
+            for (int j = 0; j < 8; j++) {
+                int col = endRow + j;
+                if (board[row].charAt(col) != answer[row%2].charAt(j)){
+                    count++;
+                }
+            }
+        }
+        return Math.min(count, 64 - count);
+    }
+}
+
