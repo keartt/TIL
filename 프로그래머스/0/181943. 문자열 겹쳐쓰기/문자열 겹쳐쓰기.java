5d9cc72b6139057e2fa27b8deb227e8d44ed00a5
keartt <83094369+keartt@users.noreply.github.com>
2024-07-16 15:41:20 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.26 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder answer = new StringBuilder(my_string);
+        
+        int ovrStrLen = overwrite_string.length();
+        for (int i = 0; i < ovrStrLen; i++) {
+            answer.setCharAt(s + i, overwrite_string.charAt(i));
+        }
+        
+        return answer.toString();
+    }
+}
