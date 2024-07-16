04c610fa9f8ed8611d26c3bd4c2ef69b3ff4c694
keartt <83094369+keartt@users.noreply.github.com>
2024-07-16 15:38:22 +0900

    [level 1] Title: K번째수, Time: 0.31 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0] -1;
+            int end = commands[i][1] -1;
+            int index = commands[i][2] -1;
+
+            List<Integer> list = new ArrayList<>();
+            for (int j = start; j <= end; j++) {
+                list.add(array[j]);
+            }
+            Collections.sort(list);
+            answer[i] = list.get(index);
+        }
+        return answer;
+    }
+}
