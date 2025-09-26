cc33fe549e3a9581393032ed13d98f730dbeca89
keartt <83094369+keartt@users.noreply.github.com>
2025-09-26 12:22:22 +0900

    [Bronze V] 8393 : 합  - 구현

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int T = Integer.parseInt(br.readLine());
+        int sum = 0;
+       for(int i = 1; i <=  T; i++){
+           sum += i;
+       }
+        System.out.println(sum);
+    }
+ 
+}
